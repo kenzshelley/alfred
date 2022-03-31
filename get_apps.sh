011a2a6f18366b788e003c9482e7e4a3c21a1125
Mackenzie Shelley <mshelley@squareup.com>
2022-03-31 10:52:08 -0400

    fix finding python installed with pyenv

--- get_apps.sh
+++ get_apps.sh
@@ -2,12 +2,17 @@
 
 set -e
 
+mkdir -p /tmp/alfred
+
 # Fetch data if not fetched. This gets deleted once per day by a cron cause its easy.
 if [ ! -f "/tmp/alfred/mshelley.json" ];
 then
   # Fetch my data
-  beyond-curl https://registry.sqprod.co/api/v1/user/mshelley.json > /tmp/mshelley.json
+  beyond-curl https://registry.sqprod.co/api/v1/user/mshelley.json > /tmp/alfred/mshelley.json
 fi
 
+# Make it so alfred can find python installed using pyenv
+eval "$(pyenv init --path)"
+
 # Hackily parse it to get my apps
-python /Users/mshelley/Development/alfred/get_apps.py /tmp/mshelley.json
+python /Users/mshelley/Development/alfred/get_apps.py /tmp/alfred/mshelley.json
